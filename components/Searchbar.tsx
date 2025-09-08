6ee8e79504966f41faf19afdaacd65e9e918f703
Kacy Do <contact@kacydo.dev>
2025-09-08 14:00:12 +1000

    add error boundary for Searchbar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,20 +1,57 @@
+'use client';
+
 import { submitForm } from '@/app/actions';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
 
 export default function Searchbar() {
 	// TODO: search bar display current query word. Zustand?
 	// TODO: error boundary for query NaN, undefined, minChar
+	const [isDirty, setIsDirty] = useState<boolean | null>(null);
+	const [input, setInput] = useState<string | null>(null);
+	const [error, setError] = useState<string | null>(null);
+
+	// Accepts only alphabets, space and hyphens
+	const regex = /^[a-zA-Z- ]*$/gm;
+
+	function handleOnBlur() {
+		setIsDirty(true);
+		if (!input || !input.length) {
+			setError('Please enter at least 1 character');
+		}
+	}
+
+	function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const formInput = e.target.value;
+
+		if (isDirty && !formInput.match(regex)) {
+			setError(`Only letters and hyphens (-) are accepted`);
+		} else if (isDirty && formInput.match(regex)) {
+			setError(null);
+		}
+
+		setInput(formInput);
+	}
+
 	return (
-		<form action={submitForm} className='join w-full'>
-			<input
-				type='text'
-				name='query'
-				placeholder='Search dictionary'
-				className='input join-item w-full pl-4 bg-base-200 rounded-l-full'
-			/>
-			<button type='submit' className='btn btn-primary join-item shadow-none'>
-				<MagnifyingGlassIcon className='size-6' />
-			</button>
+		<form action={submitForm} className='relative w-full mb-6'>
+			<div className='join w-full'>
+				<input
+					onMouseDown={() => setIsDirty(true)}
+					onBlur={handleOnBlur}
+					onChange={handleOnChange}
+					type='text'
+					name='query'
+					placeholder='Search dictionary'
+					className='input join-item w-full pl-4 bg-base-200 rounded-l-full'
+				/>
+				<button type='submit' className='btn btn-primary join-item shadow-none'>
+					<MagnifyingGlassIcon className='size-6' />
+				</button>
+			</div>
+			<p className='absolute bottom-0 ml-4 text-sm md:text-base text-error'>
+				{error ? error : ' '}
+			</p>
 		</form>
 	);
 }
